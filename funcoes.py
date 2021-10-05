eaf7a4e4e22ba0cd83b956004866af4b7e342c49
vitoria <vitoriagabrielysousa16@gmail.com>
2021-10-04 23:37:34 -0300

    máquina de estados de sepa e obaguieesse concluídas

--- funcoes.py
+++ funcoes.py
@@ -27,6 +27,7 @@ def process_string(machine, strings):
         return None
 
     state = machine[START_STATE]
+
     for string in strings:
         try:
             state = machine[TRANSITION_FUNCTION][state][string]
@@ -36,22 +37,54 @@ def process_string(machine, strings):
     return state in machine[FINAL_STATES]
 
 
-def sepa(entrada, reservadas, simbolos, numericos, tipos, listavariaveis):
-    alphabet = reservadas + simbolos + numericos + tipos + listavariaveis
-    print(f'alfabeto: {alphabet}')
+def sepa(entrada):
 
-    trans_func = {'q0': {'sepa': 'c0'},
-                  'c0': {'(': 'c1'},
-                  'c1': {tipos[0]: 'c2', listavariaveis[0]: 'c3'},
-                  'c2': {listavariaveis[0]: 'c3'},
-                  'c3': {simbolos[1]: 'c4'},
-                  'c4': {listavariaveis[0]: 'c5', numericos[0]: 'c6'},
-                  'c5': {')': 'c7'},
-                  'c6': {')': 'c7'},
-                  'c7': {'{': 'c8'},
-                  'c8': {'}': 'q0'}}
+    alphabet = ['reservada', 'tipo', 'símbolo', 'variável', 'número']
 
-    #print(f'trans_func: {trans_func}')
+    trans_func = {'q0': {'reservada': 'c0'},
+                  'c0': {'símbolo': 'c1'},
+                  'c1': {'tipo': 'c2', 'variável': 'c3', 'número': 'c3'},
+                  'c2': {'variável': 'c3'},
+                  'c3': {'símbolo': 'c4'},
+                  'c4': {'número': 'c6', 'símbolo': 'c5', 'variável': 'c7'},
+                  'c5': {'número': 'c6', 'variável': 'c7'},
+                  'c6': {'símbolo': 'c8'},
+                  'c7': {'símbolo': 'c8'},
+                  'c8': {'símbolo': 'c9'},
+                  'c9': {'símbolo': 'q0'}}
+
+    start_state = 'q0'
+    final_states = (['q0'])
+    machine = (alphabet, trans_func, start_state, final_states)
+    strings = entrada
+
+    result = process_string(machine, strings)
+
+    if result is not None:
+        for string in strings:
+            print(f'{result}: {string}')
+    else:
+        print("\nERROR")
+
+
+def obaguieesse(entrada):
+
+    alphabet = ['reservada', 'tipo', 'símbolo', 'variável', 'número', 'texto']
+
+    trans_func = {'q0': {'reservada': 'p0'},
+                  'p0': {'símbolo': 'p1'},
+                  'p1': {'símbolo': 'p2', 'variável': 'p4', 'símbolo': 'p6'},
+                  'p2': {'texto': 'p3'},
+                  'p3': {'símbolo': 'c4'},
+                  'p4': {'símbolo': 'p5'},
+                  'p5': {'símbolo': 'q0'},
+                  'p6': {'símbolo': 'p7'},
+                  'p7': {'texto': 'p8', 'símbolo': 'p9'},
+                  'p8': {'símbolo': 'p9', 'símbolo': 'p12'},
+                  'p9': {'variável': 'p10'},
+                  'p10': {'símbolo': 'p11'},
+                  'p11': {'texto': 'p8', 'símbolo': 'p9', 'símbolo': 'p12'},
+                  'p12': {'símbolo': 'p5'}}
 
     start_state = 'q0'
     final_states = (['q0'])
