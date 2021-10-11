bc0a14f4ff348baec0c1b75391d8d925b9a20eed
Al-Maffra <allan.maffra@gmail.com>
2021-10-10 23:33:34 -0300

    Atualização do Ocorre (laço de repetição) Atualização do vaivenafita (equivalente ao scanf)
    Mudanças: Alphabet atualizado para rótulos ou caracter fixo pensando ja na semântica.
    
    Teste realizado com as entradas esperadas e situações com erro.

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,6 @@
+(ALPHABET, TRANSITION_FUNCTION, START_STATE, FINAL_STATES) = range(4)
+
+
 def manipularArquivo(arquivo, tipo):
     abrirarquivo = (open(arquivo, tipo))
     lerarquivo = abrirarquivo.read()
@@ -13,7 +16,6 @@ def gerarLista(arquivo, tipo):
 
 
 def validate_string(string, machine):
-
     if string in machine[ALPHABET]:
         return True
     elif string in machine[ALPHABET][2][0]:
@@ -21,35 +23,27 @@ def validate_string(string, machine):
     return False
 
 
-(
-    ALPHABET,
-    TRANSITION_FUNCTION,
-    START_STATE,
-    FINAL_STATES
-) = range(4)
-
-
-def process_string(machine, strings, listavar):
+def process_string(machine, strings):
     state = machine[START_STATE]
     for char in strings:
-        print(char)
+        # print(char)
         if not validate_string(char, machine):
-            return None  # Processamento do automato.
+            return None
 
         try:
             state = machine[TRANSITION_FUNCTION][state][char]
         except:
             return None
-        print(state)
+        # print(state)
 
     return state in machine[FINAL_STATES]
 
 
-def vaivenafita(inp, listavar):
-    alphabet = ['vaivenafita', '(', listavar[0], ')', ';']
+def vaivenafita(inp):
+    alphabet = ['vaivenafita', '(', 'variável', ')', ';']
     trans_func = {'q0': {'vaivenafita': 'sc1'},
                   'sc1': {'(': 'sc2'},
-                  'sc2': {listavar[0]: 'sc3'},
+                  'sc2': {'variável': 'sc3'},
                   'sc3': {')': 'sc4'},
                   'sc4': {';': 'q0'}}
 
@@ -57,7 +51,45 @@ def vaivenafita(inp, listavar):
     final_states = {'q0'}
     machine = (alphabet, trans_func, start_state, final_states)
 
-    result = process_string(machine, inp, listavar)
+    result = process_string(machine, inp)
+    if result is not None:
+        print(f'Machine: {machine}')
+    else:
+        print('Fora de ordem')
+
+
+def ocorre(inp):
+    alphabet = ['ocorre', '(', 'tipo', 'variável', ';', '=', 'número', '<', '>', ')', '{', '}', '+']
+    trans_func = {'q0': {'ocorre': 'f0'},
+                  'f0': {'(': 'f1'},
+                  'f1': {'tipo': 'f2', ';': 'f7', 'variável': 'f3'},
+                  'f2': {'variável': 'f3'},
+                  'f3': {'=': 'f4'},
+                  'f4': {'número': 'f5', 'variável': 'f6'},
+                  'f5': {';': 'f7'},
+                  'f6': {';': 'f7'},
+                  'f7': {'variável': 'f8'},
+                  'f8': {'<': 'f9', '>': 'f9'},
+                  'f9': {'=': 'f10', 'número': 'f11', 'variável': 'f12'},
+                  'f10': {'número': 'f11', 'variável': 'f12'},
+                  'f11': {';': 'f13'},
+                  'f12': {';': 'f13'},
+                  'f13': {'variável': 'f14', '+': 'f17', '-': 'f17'},
+                  'f14': {'+': 'f15', '-': 'f15'},
+                  'f15': {'=': 'f16', '+': 'f20', '-': 'f20'},
+                  'f16': {'variável': 'f19', 'número': 'f20'},
+                  'f17': {'+': 'f18', '-': 'f18'},
+                  'f18': {'variável': 'f20'},
+                  'f19': {')': 'f21'},
+                  'f20': {')': 'f21'},
+                  'f21': {'{': 'f22'},
+                  'f22': {'}': 's6'}}
+
+    start_state = 'q0'
+    final_states = {'s6'}
+    machine = (alphabet, trans_func, start_state, final_states)
+
+    result = process_string(machine, inp)
     if result is not None:
         print(f'Machine: {machine}')
     else:
--- main.py
+++ main.py
@@ -8,10 +8,22 @@ listatipos = gerarLista('tipos.cha', 'r')
 entrada = input().replace(" ", "")
 lista = entrada[0]
 
+# ocorre(        ; x<5;  x++)
+# ocorre(     x=0; x<5;  x++)
+# ocorre(mano x=x; x<x;  x++)
+# ocorre(mano x=0; x<5;  ++x)
+# ocorre(mano x=0; x<=5; x++)
+# ocorre(mano x=0; x<=5; x+=2)
+# ocorre(mano x=0; x<=5; x+=x)
+
+# vaivenafita(x);
+
+
 flag = False
 indice = 0
 listavariaveis = []
 listalexico = []
+inp = []
 
 while indice < len(entrada):
     print(f'{lista}, indice: {indice}')
@@ -19,30 +31,35 @@ while indice < len(entrada):
     if lista in listareservadas and lista not in listatipos:
         listalexico.append(f'Símbolo [frag = {lista}, tipo = palavra reservada]')
         flag = True
+        inp.append(lista)
 
     elif lista in listasimbolos:
         listalexico.append(f'Símbolo [frag = {lista}, tipo = símbolo]')
         flag = True
+        inp.append(lista)
 
     elif lista in listavariaveis:
         listalexico.append(f'Símbolo [frag = {lista}, tipo = variável]')
         flag = True
+        inp.append('variável')
 
     elif lista.isnumeric():
         listalexico.append(f'Símbolo [frag = {lista}, tipo = numérico]')
         flag = True
+        inp.append('número')
 
     elif lista in listatipos:
         listalexico.append(f'Símbolo [frag = {lista}, tipo = tipo]')
         lista = entrada[indice + 1]
+        flag = True
+        inp.append('tipo')
+        # if lista.isnumeric():
+        #     listalexico.append(f'Símbolo [frag = {lista}, tipo = numérico]')
 
-        if lista.isnumeric():
-            listalexico.append(f'Símbolo [frag = {lista}, tipo = numérico]')
-
-        else:
-            listalexico.append(f'Símbolo [frag = {lista}, tipo = variável]')
-            listavariaveis.append(lista)
-        indice += 1
+    elif entrada[indice + 1] in listasimbolos:
+        listalexico.append(f'Símbolo [frag = {lista}, tipo = variável]')
+        listavariaveis.append(lista)
+        inp.append('variável')
         flag = True
 
     else:
@@ -50,13 +67,21 @@ while indice < len(entrada):
             lista = lista + entrada[indice + 1]
     if flag == True:
         if indice < len(entrada) - 1:
-            #print(f'passou {lista}, {indice}, {len(entrada)}')
+            # print(f'passou {lista}, {indice}, {len(entrada)}')
             lista = entrada[indice + 1]
         else:
-            #print("pass")
+            # print("pass")
             pass
     indice += 1
 
-print()
+# print(inp)
+
 stringlexico = ', '.join(map(str, listalexico))
 print(f'Léxico: {stringlexico}')
+
+if inp[0] == 'vaivenafita':
+    print('entrou vaivenafita')
+    vaivenafita(inp)
+else:
+    print('entrou ocorre')
+    ocorre(inp)
