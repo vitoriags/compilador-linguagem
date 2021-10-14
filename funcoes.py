623e7128545ec6467fbbb0bda893ef043f11d1c5
vitoria <vitoriagabrielysousa16@gmail.com>
2021-10-13 23:08:43 -0300

    estruturação do código para merge

--- funcoes.py
+++ funcoes.py
@@ -90,3 +90,62 @@ def obaguieesse(entrada):
             print(f'{result}: {string}')
     else:
         print("Entrada não Válida")
+
+
+def vaivenafita(entrada):
+    alphabet = ['vaivenafita', '(', 'variável', ')', ';']
+    trans_func = {'q0': {'vaivenafita': 'sc1'},
+                  'sc1': {'(': 'sc2'},
+                  'sc2': {'variável': 'sc3'},
+                  'sc3': {')': 'sc4'},
+                  'sc4': {';': 'q0'}}
+
+    start_state = 'q0'
+    final_states = {'q0'}
+    machine = (alphabet, trans_func, start_state, final_states)
+
+    result = process_string(machine, entrada)
+    if result is not None and result is not False:
+        for string in entrada:
+            print(f'{result}: {string}')
+    else:
+        print("Entrada não válida")
+
+
+def ocorre(entrada):
+    alphabet = ['ocorre', '(', 'tipo', 'variável', ';', '=', 'número', '<', '>', ')', '{', '}', '+']
+    trans_func = {'q0': {'ocorre': 'f0'},
+                  'f0': {'(': 'f1'},
+                  'f1': {'tipo': 'f2', ';': 'f7', 'variável': 'f3'},
+                  'f2': {'variável': 'f3'},
+                  'f3': {'=': 'f4'},
+                  'f4': {'número': 'f5', 'variável': 'f6'},
+                  'f5': {';': 'f7'},
+                  'f6': {';': 'f7'},
+                  'f7': {'variável': 'f8'},
+                  'f8': {'<': 'f9', '>': 'f9'},
+                  'f9': {'=': 'f10', 'número': 'f11', 'variável': 'f12'},
+                  'f10': {'número': 'f11', 'variável': 'f12'},
+                  'f11': {';': 'f13'},
+                  'f12': {';': 'f13'},
+                  'f13': {'variável': 'f14', '+': 'f17', '-': 'f17'},
+                  'f14': {'+': 'f15', '-': 'f15'},
+                  'f15': {'=': 'f16', '+': 'f20', '-': 'f20'},
+                  'f16': {'variável': 'f19', 'número': 'f20'},
+                  'f17': {'+': 'f18', '-': 'f18'},
+                  'f18': {'variável': 'f20'},
+                  'f19': {')': 'f21'},
+                  'f20': {')': 'f21'},
+                  'f21': {'{': 'f22'},
+                  'f22': {'}': 's6'}}
+
+    start_state = 'q0'
+    final_states = {'s6'}
+    machine = (alphabet, trans_func, start_state, final_states)
+
+    result = process_string(machine, entrada)
+    if result is not None and result is not False:
+        for string in entrada:
+            print(f'{result}: {string}')
+    else:
+        print("Entrada não válida")
